434499ffcf0366b5b025a77de0c633432af155d1
mtn2bay <mikemroy@gmail.com>
2017-05-31 22:30:25 -0400

    Process user input, palindrome algorithim

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -3,10 +3,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SearchService {
 
-  checkCondition(userValues) {
-    console.log(userValues);
+  userValues = [];
+
+  result = "";
+
+  isPalindrome(n) {
+    return n.toString().split('').reverse().join('') === n.toString();
   }
 
-  constructor() { }
+  checkCondition(userValues) {
+
+    var workingNum,
+      endPoint;
+
+    if (+userValues[0] > +userValues[1]) {
+      workingNum = +userValues[0];
+      endPoint = +userValues[1];
+    } else {
+      workingNum = +userValues[1];
+      endPoint = +userValues[0];
+    }
+
+    while (workingNum >= endPoint) {
+      if (this.isPalindrome(workingNum)) {
+        this.result = workingNum;
+        break;
+      } else {
+        workingNum--;
+      }
+    }
+  }
+
 
 }
